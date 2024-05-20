d71c56d334fcf39488a76ccb7c33d976cc7d7178
Seonggon, Cho <77792994+jmsmg@users.noreply.github.com>
2024-05-20 11:55:35 +0000

    ok

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -25,6 +25,42 @@ class MutantStack : public std::stack<T>
 			return (this->c.end());
 		}
 
+		typedef typename	MutantStack<T>::container_type::const_iterator const_iterator;
+
+		const_iterator	cbegin(void)
+		{
+			return (this->c.cbegin());
+		}
+
+		const_iterator	cend(void)
+		{
+			return (this->c.cend());
+		}
+
+		typedef typename	MutantStack<T>::container_type::reverse_iterator reverse_iterator;
+
+		reverse_iterator	rbegin(void)
+		{
+			return (this->c.rbegin());
+		}
+
+		reverse_iterator	rend(void)
+		{
+			return (this->c.rend());
+		}
+
+		typedef typename	MutantStack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+		const_reverse_iterator	crbegin(void)
+		{
+			return (this->c.crbegin());
+		}
+
+		const_reverse_iterator	crend(void)
+		{
+			return (this->c.crend());
+		}
+
 };
 
 #endif
