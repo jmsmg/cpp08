ebdda36323cf6bbb76034986d6a58bc8198974b1
jmsmg <jmsmg1@me.com>
2024-05-22 08:57:03 +0900

    fix test case

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -31,5 +31,38 @@ int main()
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+	std::cout << "---------------- list ---------------" << std::endl;
+
+	std::list<int> list;
+
+	list.push_back(5);
+	list.push_back(17);
+
+	std::cout << list.back() << std::endl;
+
+	list.pop_back();
+
+	std::cout << list.size() << std::endl;
+
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+	list.push_back(0);
+
+	std::list<int>::iterator it1 = list.begin();
+	std::list<int>::iterator ite1 = list.end();
+
+	++it1;
+
+	--it1;
+
+	while (it1 != ite1)
+	{
+		std::cout << *it1 << std::endl;
+		++it1;
+	}
+	std::list<int> l(list);
+
 	return (0);
 }
